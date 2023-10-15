cacca3e3ab5aba0f10e302886f56fcf460cad743
mainul96 <110577655+mainul96@users.noreply.github.com>
2023-10-15 10:07:59 +0530

    add toost sucefully

--- src/components/Excersize/Excersize.js
+++ src/components/Excersize/Excersize.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './Excersize.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const Excersize = ({cart}) => {
+const Excersize = ({ cart }) => {
+    const notify = () => toast("Wow so easy !");
     //console.log(cart);
     let total = 0;
-    for(const sport of cart){
+    for (const sport of cart) {
         total = total + sport.time;
     }
     return (
-        <div className='excersize-details'>
-            <h2>Excersize Details:{cart.length}</h2>
-            <p>Total:{total}</p>
+        <div>
+            <h1>Excersize Details</h1>
+            <div className='excersize-details'>
+                <h2>Excersize Details:{cart.length}</h2>
+                <p>Total:{total}</p>
+            </div>
+            <button className='btn-toost' onClick={notify}>Add Activities</button>
+            <ToastContainer />
         </div>
     );
 };
